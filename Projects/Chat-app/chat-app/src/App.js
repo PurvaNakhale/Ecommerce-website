4f76bcc247db05af8cd49b66c00ba26038aef2cd
purva26 <purvanakhale@gmail.com>
2022-01-18 22:52:13 +0530

    created public and private route

--- Projects/Chat-app/chat-app/src/App.js
+++ Projects/Chat-app/chat-app/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
-
+import { Switch } from 'react-router';
+import Home from './Pages/Home';
 import 'rsuite/dist/rsuite.min.css';
+import PrivateRoute from './components/PrivateRoute';
+import SignIn from './Pages/SignIn';
 import './styles/main.scss';
+import PublicRoute from './components/PublicRoute';
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <Switch>
+      <PublicRoute path="/signin">
+        <SignIn />
+      </PublicRoute>
+      <PrivateRoute path="/">
+        <Home />
+      </PrivateRoute>
+    </Switch>
+  );
 }
 
 export default App;
---
+++ Projects/Chat-app/chat-app/src/components/PublicRoute.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Redirect, Route } from 'react-router/cjs/react-router.min';
+
+const PublicRoute = ({ children, ...routeProps }) => {
+  const profile = false;
+
+  if (profile) {
+    return <Redirect to="/" />;
+  }
+
+  return <Route {...routeProps}>{children}</Route>;
+};
+
+export default PublicRoute;
