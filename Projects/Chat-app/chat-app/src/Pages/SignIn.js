9855d50defb902dddc441e33efb2c21910691c89
purva26 <purvanakhale@gmail.com>
2022-01-20 11:22:17 +0530

    scaffold the app

--- Projects/Chat-app/chat-app/src/Pages/SignIn.js
+++ Projects/Chat-app/chat-app/src/Pages/SignIn.js
@@ -1,7 +1,61 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-expressions */
 import React from 'react';
+import firebase from 'firebase/app';
+import { Container, Grid, Row, Panel, Col, Button, IconButton } from 'rsuite';
+import { auth, database } from '../mics/firebase';
 
 const SignIn = () => {
-  return <div>Sign In</div>;
+  const signInWithProvider = async provider => {
+    try {
+      const { additionalUserInfo, user } = await auth.signInWithPopup(provider);
+
+      if (additionalUserInfo.isNewUser) {
+        await database.ref(`/profile/${user.uid}`).set({
+          name: user.displayName,
+          createdAt: firebase.database.ServerValue.TIMESTAMP,
+        });
+      }
+      Alert.success('Signed in', 4000);
+    } catch (err) {
+      Alert.error(err.message, 4000);
+    }
+  };
+
+  const onFacebookSignIn = () => {
+    signInWithProvider(new firebase.auth.FacebookAuthProvider());
+  };
+
+  const onGoogleSignIn = () => {
+    signInWithProvider(new firebase.auth.GoogleAuthProvider());
+  };
+
+  return (
+    <Container>
+      <Grid className="mt-page">
+        <Row>
+          <Col xs={24} md={12} mdOffset={6}>
+            <Panel>
+              <div className="text-center">
+                <h2>Welcome to Chat</h2>
+                <p>Progressive chat platform for neophytes</p>
+              </div>
+
+              <div className="mt-3">
+                <Button block color="blue" onClick={onFacebookSignIn}>
+                  <IconButton icon="facebook" /> Continue with Facebook
+                </Button>
+
+                <Button block color="green" onClick={onGoogleSignIn}>
+                  <IconButton icon="google" /> Continue with Google
+                </Button>
+              </div>
+            </Panel>
+          </Col>
+        </Row>
+      </Grid>
+    </Container>
+  );
 };
 
 export default SignIn;
