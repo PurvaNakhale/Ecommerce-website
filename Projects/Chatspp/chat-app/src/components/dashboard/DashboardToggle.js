934790d2bc24414c1b4542d45528f2934237f974
purva26 <purvanakhale@gmail.com>
2022-01-21 23:58:19 +0530

    building Dashboard

--- Projects/Chatspp/chat-app/src/components/dashboard/DashboardToggle.js
+++ Projects/Chatspp/chat-app/src/components/dashboard/DashboardToggle.js
@@ -1,12 +1,21 @@
 /* eslint-disable import/no-unresolved */
-import React from 'react';
-import { Button, Icon, Drawer } from 'rsuite';
+import React, { useCallback } from 'react';
+import { Button, Icon, Drawer, Alert } from 'rsuite';
 import Dashboard from '.';
 import { useMediaQuery, useModalState } from '../../mics/custom-hooks';
+import { auth } from '../../mics/firebase';
 
 const DashboardToggle = () => {
   const { isOpen, close, open } = useModalState();
   const isMobile = useMediaQuery('(max-width: 992px)');
+
+  const onSignOut = useCallback(() => {
+    auth.signOut();
+
+    Alert.info('Signed out', 4000);
+
+    close();
+  }, [close]);
 
   return (
     <>
@@ -14,7 +23,7 @@ const DashboardToggle = () => {
         <Icon icon="dashboard" /> Dashboard
       </Button>
       <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
-        <Dashboard />
+        <Dashboard onSignOut={onSignOut} />
       </Drawer>
     </>
   );
