2f85c4eca9f19a2a4010a84d0de168aece402a34
purva26 <purvanakhale@gmail.com>
2022-01-25 15:04:50 +0530

    scoffold our app

--- Projects/Chat-app/chat-app/src/App.js
+++ Projects/Chat-app/chat-app/src/App.js
@@ -1,23 +1,9 @@
 import React from 'react';
-import { Switch } from 'react-router';
-import Home from './Pages/Home';
 import 'rsuite/dist/rsuite.min.css';
-import PrivateRoute from './components/PrivateRoute';
-import SignIn from './Pages/SignIn';
 import './styles/main.scss';
-import PublicRoute from './components/PublicRoute';
 
 function App() {
-  return (
-    <Switch>
-      <PublicRoute path="/signin">
-        <SignIn />
-      </PublicRoute>
-      <PrivateRoute path="/">
-        <Home />
-      </PrivateRoute>
-    </Switch>
-  );
+  return <div>Hello</div>;
 }
 
 export default App;
